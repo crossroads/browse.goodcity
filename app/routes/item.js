4d369e5816367872e0220080d9ef90da3729b4bd
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2018-08-13 16:49:01 +0530

    HotFix: Item not available pop-up shows up even if item is available

--- app/routes/item.js
+++ app/routes/item.js
@@ -2,28 +2,9 @@ import PublicRoute from './browse_pages';
 import Ember from 'ember';
 
 export default PublicRoute.extend({
-  messageBox: Ember.inject.service(),
-  isAvailable: false,
-  transition: null,
-
-  beforeModel(transition) {
-    this._super(...arguments);
-    this.set('transition', transition);
-  },
 
   model(params) {
     return this.store.peekRecord('item', params["id"]);
-  },
-
-  afterModel(model) {
-    var isItemUnavailable = model.get('isUnavailableAndDesignated');
-    if(isItemUnavailable && isItemUnavailable !== null) {
-      this.get('transition').abort();
-      this.get('messageBox').alert(this.get('i18n').t('cart_content.unavailable'),
-      () => {
-        this.transitionTo('/browse');
-      });
-    }
   },
 
   setupController(controller, model){
