ad963a7b171a111ed8f5cecca0f8d1fe9f8d260e
AbulAsar <abul.a@kiprosh.com>
2018-09-25 13:45:39 +0530

    test case added for redirecting user to browse page after submitting account_details

--- tests/acceptance/account_details-test.js
+++ tests/acceptance/account_details-test.js
@@ -18,6 +18,7 @@ module('Acceptance | Account Details Page', {
     order = make("order", { state: "draft", created_by_id: user.id });
     gogo_van = make("gogovan_transport");
     gcOrganisations = make("gc_organisation", {nameEn: "GCC club"});
+
     $.mockjax({url: "/api/v1/available_*", type: 'GET', status: 200, responseText:["2018-06-14", "2018-06-15", "2018-06-16", "2018-06-19", "2018-06-20", "2018-06-21"]});
     mockFindAll("gogovan_transport").returns({json: {gogovan_transports: [gogo_van.toJSON({includeId: true})]}});
 
@@ -29,10 +30,19 @@ module('Acceptance | Account Details Page', {
         organisations_users: [organisationsUser.toJSON({includeId: true})]
       }});
 
-    $.mockjax({url: "api/v1/gc_org*", responseText: {
-       gc_organisations: [gcOrganisations.toJSON({includeId: true})]
+    $.mockjax({url: "api/v1/gc_org*", type: 'GET', responseText: {
+       meta :{ total_pages:1, search: "club"},
+       gc_organisations: [gcOrganisations.toJSON({includeId: true}), organisation.toJSON({includeId: true})],
+       organisations: [gcOrganisations.toJSON({includeId: true}), organisation.toJSON({includeId: true})],
     }});
     mockFindAll('order').returns({ json: {orders: [order.toJSON({includeId: true})], packages: [pkg.toJSON({includeId: true})], orders_packages: [ordersPackage.toJSON({includeId: true})]}});
+
+    $.mockjax({url:"/api/v1/org*", type: 'POST', status: 200,responseText:{
+        users: [user.toJSON({ includeId: true })],
+        organisations: [organisation.toJSON({includeId: true})],
+        organisations_users: [organisationsUser.toJSON({includeId: true})]
+      }
+    });
   },
 
   afterEach: function() {
@@ -53,7 +63,17 @@ test("Clicking organisations from search organisation page fills organisatin det
   });
 });
 
-test("User redirects to search_organisation page on clicking ", function(assert){
+test("After saving user details user gets redirected to browse page", function(assert) {
+  visit('/account_details');
+  andThen(function(){
+    click(".expand_button");
+  });
+  andThen(function() {
+    assert.equal(currentURL(), "/browse");
+  });
+});
+
+test("User redirects to search_organisation page on clicking Organisation Name Input", function(assert){
   assert.expect(2);
   visit('/account_details');
   andThen(function(){
