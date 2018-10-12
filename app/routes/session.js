b24f8e92ed3bb4126362033dbb7f90339d4932d7
namrataukirde <namrata.ukirde@kiprosh.com>
2018-10-12 16:10:34 +0530

    login based on condition

--- app/routes/session.js
+++ app/routes/session.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 
   beforeModel() {
+    // this.controller.set('bookingAppointment', params['bookingAppointment']);
     if (this.session.get('isLoggedIn')) {
       this.transitionTo('/');
     } else {
