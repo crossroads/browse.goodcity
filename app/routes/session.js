b60ea1517b59ecbc4df9a41ef27c6f339dc1f8d9
namrataukirde <namrata.ukirde@kiprosh.com>
2018-10-15 16:48:03 +0530

    send bookAppointment param to authenticate

--- app/routes/session.js
+++ app/routes/session.js
@@ -3,7 +3,6 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 
   beforeModel() {
-    // this.controller.set('bookingAppointment', params['bookingAppointment']);
     if (this.session.get('isLoggedIn')) {
       this.transitionTo('/');
     } else {
