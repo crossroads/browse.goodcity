ad223e48633788331dafb4bbca8e6db0b4853202
namrataukirde <namrata.ukirde@kiprosh.com>
2018-10-18 18:35:16 +0530

    client information page

--- app/controllers/request_purpose.js
+++ app/controllers/request_purpose.js
@@ -3,8 +3,14 @@ import AjaxPromise from 'browse/utils/ajax-promise';
 const { getOwner } = Ember;
 
 export default Ember.Controller.extend({
+  peopleCount: null,
+  description: "",
+  selectedId: null,
+  isSelfSelected: Ember.computed.equal("selectedId", "own"),
+
+
   actions: {
-    saveItem(){
+    saveBenificieryDetails(){
       console.log('abc');
     }
   }
