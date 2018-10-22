09401c46f20bd64558581de4171125fb1e6e22c8
namrataukirde <namrata.ukirde@kiprosh.com>
2018-10-23 02:24:26 +0530

    add rbcl type

--- app/controllers/client_information.js
+++ app/controllers/client_information.js
@@ -3,6 +3,23 @@ import AjaxPromise from 'browse/utils/ajax-promise';
 const { getOwner } = Ember;
 
 export default Ember.Controller.extend({
+  isHkIdSelected: Ember.computed.equal("selectedId", "hkId"),
+
+  titles: Ember.computed(function() {
+    let translation = this.get("i18n");
+    let mr = translation.t("account.user_title.mr");
+    let mrs = translation.t("account.user_title.mrs");
+    let miss = translation.t("account.user_title.miss");
+    let ms = translation.t("account.user_title.ms");
+
+    return [
+      { name: mr, id: "Mr" },
+      { name: mrs, id: "Mrs" },
+      { name: miss, id: "Miss" },
+      { name: ms, id: "Ms" }
+    ];
+  }),
+
   actions: {
     saveClientDetails(){
       console.log('client info controller');
--- app/routes/client_information.js
+++ app/routes/client_information.js
@@ -2,5 +2,12 @@ import AuthorizeRoute from './authorize';
 import Ember from 'ember';
 
 export default AuthorizeRoute.extend({
+  setUpFormData(model, controller) {
+    controller.set('selectedId', "hkId");
+  },
 
+  setupController(controller, model) {
+    this._super(...arguments);
+    this.setUpFormData(model, controller);
+  }
 });
