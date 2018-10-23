989919e6ce9b4745bdcbe1c704f0c014ebe3eeff
namrataukirde <namrata.ukirde@kiprosh.com>
2018-10-23 13:49:27 +0530

    adding goodcity_details under order

---
+++ app/controllers/order/goods_details.js
@@ -0,0 +1,7 @@
+import Ember from "ember";
+import config from '../../config/environment';
+import AjaxPromise from 'browse/utils/ajax-promise';
+const { getOwner } = Ember;
+
+export default Ember.Controller.extend({
+});
