cabc953678cc8662b863bee62e017444058c1dc1
macLaptop <maclaptop@macLaptops-MacBook-Pro.local>
2018-11-01 13:22:31 +0530

    appointment page changes

--- app/routes/order/confirm_booking.js
+++ app/routes/order/confirm_booking.js
@@ -1,18 +1,13 @@
 import AuthorizeRoute from './../authorize';
+import Ember from 'ember';
 
 export default AuthorizeRoute.extend({
-  model() {
+	model() {
     var orderId = this.paramsFor('order').order_id;
     return this.store.peekRecord('order', orderId);
   },
 
-  setUpFormData(model, controller) {
-    controller.set('selectedId', "self");
-    controller.set('selectedTimeId', "11:00am");
-  },
-
   setupController(controller, model) {
     this._super(...arguments);
-    this.setUpFormData(model, controller);
   }
 });
