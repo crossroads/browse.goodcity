eb1581fe0c711f097c9f25129c2424f15a85de31
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2018-11-14 16:23:41 +0530

    Available dates issue fixed for calendar

--- app/routes/order/appointment_details.js
+++ app/routes/order/appointment_details.js
@@ -1,14 +1,18 @@
 import AuthorizeRoute from './../authorize';
+import AjaxPromise from 'browse/utils/ajax-promise';
 
 export default AuthorizeRoute.extend({
   model() {
-    var orderId = this.paramsFor('order').order_id;
-    return this.store.peekRecord('order', orderId);
+    return Ember.RSVP.hash({
+      availableDates: new AjaxPromise("/available_dates", "GET", this.get('session.authToken'), {schedule_days: 40}),
+      order: this.store.peekRecord('order', this.paramsFor('order').order_id)
+    });
   },
 
   setUpFormData(model, controller) {
     controller.set('selectedId', "self");
     controller.set('selectedTimeId', "11:00am");
+    controller.set('available_dates', model.availableDates)
   },
 
   setupController(controller, model) {
