3d49f41eb9222e7d039aa075c8c6f492f209b729
Patrick R <patrixr@Patricks-MacBook-Pro.local>
2018-11-22 17:02:44 +0530

    FIX: check for undefined variable

--- app/adapters/order.js
+++ app/adapters/order.js
@@ -1,8 +1,9 @@
 import ApplicationAdapter from './application';
+import _ from 'lodash';
 
 export default ApplicationAdapter.extend({
   findRecord(store, type, id, snapshot) {
-    if (snapshot.adapterOptions.includePackages) {
+    if (_.has(snapshot, 'adapterOptions.includePackages')) {
       let url = this.buildURL(type.modelName, id, snapshot, 'findRecord');
       let query = { include_packages: snapshot.adapterOptions.includePackages };
       return this.ajax(url, 'GET', { data: query });
