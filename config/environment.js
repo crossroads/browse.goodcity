5b41d4a518bf1b3c31c0568beb27659b629e740d
Darshan Chauhan <darshan.chauhan@kiprosh.com>
2018-11-26 10:25:57 +0530

    Browse staging pointing to prod api fix

--- config/environment.js
+++ config/environment.js
@@ -130,7 +130,7 @@ module.exports = function(environment) {
       'https://api.cloudinary.com'
     ].join(' ');
 
-    if (process.env.staging === 'true') {
+    if ((process.env.staging || process.env.STAGING) === 'true') {
       ENV.staging = true;
       ENV.APP.API_HOST_URL = 'https://api-staging.goodcity.hk';
       ENV.APP.SOCKETIO_WEBSERVICE_URL = 'https://socket-staging.goodcity.hk:81/goodcity';
