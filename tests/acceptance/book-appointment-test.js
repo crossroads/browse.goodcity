15b988f041cb5e2979dd6644923bfed7ade81e26
Abhinav Garg <abhinav.g@kiprosh.com>
2018-12-03 18:38:40 +0530

    add some tests for client info page

--- tests/acceptance/book-appointment-test.js
+++ tests/acceptance/book-appointment-test.js
@@ -3,7 +3,7 @@ import { module, test } from 'qunit';
 import startApp from 'browse/tests/helpers/start-app';
 import { make, mockFindAll } from 'ember-data-factory-guy';
 
-var App, order, gogo_van, user, user_profile, orderPurpose, organisation, pkg, ordersPackage, mocks, goodcityRequest;
+var App, order, gogo_van, user, user_profile, orderPurpose1, orderPurpose2 , organisation, pkg, ordersPackage, mocks, goodcityRequest, benificiary, identity_types;
 
 module('Acceptance | BookAppointment', {
   beforeEach: function() {
@@ -20,6 +20,8 @@ module('Acceptance | BookAppointment', {
     "mobile": user.get('mobile'), "user_role_ids": [1] };
     goodcityRequest = {id: 1, quantity: 1, description: null, code_id: null,
       order_id: order.id, package_type_id: null};
+    benificiary = {created_by_id: user.id, first_name: "Test" ,id: 44, identity_number: "1233",identity_type_id: 1 , last_name: "John", phone_number: "+85212312312", title: null};
+    identity_types = [{id: 2, name: "Asylum Seeker Recognizance Form"}];
     mocks = [];
     $.mockjaxSettings.matchInRegistrationOrder = false;
     mocks.push(
@@ -65,7 +67,7 @@ test("should redirect to login page if user not logged in", function(assert) {
 });
 
 test("request purpose page on completely filled should redirect to Goods details if For organisation's own programs is selected", function(assert){
-  orderPurpose = {id: 1, purpose_id: 1, order_id: order.id, designation_id: 1};
+  orderPurpose1 = {id: 1, purpose_id: 1, order_id: order.id, designation_id: 1};
   assert.expect(1);
   visit('/request_purpose');
   andThen(function(){
@@ -84,7 +86,7 @@ test("request purpose page on completely filled should redirect to Goods details
     mocks.push(
       $.mockjax({url: '/api/v1/order*', type: 'POST', status: 201,responseText: {
         order: order.toJSON({includeId: true}),
-        orders_purposes: [orderPurpose],
+        orders_purposes: [orderPurpose1],
         user: user.toJSON({includeId: true})}
       })
     );
@@ -96,7 +98,6 @@ test("request purpose page on completely filled should redirect to Goods details
 });
 
 test("request purpose page should not redirect if incomplete form", function(assert){
-  orderPurpose = {id: 1, purpose_id: 1, order_id: order.id, designation_id: 1};
   assert.expect(1);
   visit('/request_purpose');
   andThen(function(){
@@ -123,7 +124,7 @@ test("should redirect to Request purpose page if user is logged in", function(as
 });
 
 test("request purpose page on completely filled should redirect to client information if For client benificiary is selected", function(assert){
-  orderPurpose = {id: 1, purpose_id: 2, order_id: order.id, designation_id: 1};
+  orderPurpose2 = {id: 1, purpose_id: 2, order_id: order.id, designation_id: 1};
   assert.expect(1);
   visit('/request_purpose');
   andThen(function(){
@@ -142,13 +143,59 @@ test("request purpose page on completely filled should redirect to client inform
     mocks.push(
       $.mockjax({url: '/api/v1/order*', type: 'POST', status: 201,responseText: {
         order: order.toJSON({includeId: true}),
-        orders_purposes: [orderPurpose],
+        orders_purposes: [orderPurpose2],
         user: user.toJSON({includeId: true})}
       })
     );
     click('.button.expand');
     andThen(function(){
       assert.equal(Ember.$('.title').text().trim(), "Client Information");
+    });
+  });
+});
+
+test("Select HkID on client info should display form for hkid", function(assert){
+  orderPurpose2 = {id: 1, purpose_id: 2, order_id: order.id, designation_id: 1};
+  assert.expect(2);
+  mocks.push(
+    $.mockjax({url: '/api/v1/order*', type: 'POST', status: 201,responseText: {
+      order: order.toJSON({includeId: true}),
+      orders_purposes: [orderPurpose2],
+      user: user.toJSON({includeId: true})}
+    })
+  );
+  let client_info_url = `/order/${order.id}/client_information`;
+  visit(client_info_url);
+  andThen(function(){
+    assert.equal(currentURL(), client_info_url);
+  });
+  andThen(function(){
+    click('.hkId');
+    andThen(function(){
+      assert.equal(Ember.$('#id-initials').text().trim(), "P12");
+    });
+  });
+});
+
+test("Select RBCL on client info should display form for rbcl", function(assert){
+  orderPurpose2 = {id: 1, purpose_id: 2, order_id: order.id, designation_id: 1};
+  assert.expect(2);
+  mocks.push(
+    $.mockjax({url: '/api/v1/order*', type: 'POST', status: 201,responseText: {
+      order: order.toJSON({includeId: true}),
+      orders_purposes: [orderPurpose2],
+      user: user.toJSON({includeId: true})}
+    })
+  );
+  let client_info_url = `/order/${order.id}/client_information`;
+  visit(client_info_url);
+  andThen(function(){
+    assert.equal(currentURL(), client_info_url);
+  });
+  andThen(function(){
+    click('.abcl');
+    andThen(function(){
+      assert.equal(Ember.$('#id-initials').text().trim(), "RBCL");
     });
   });
 });
