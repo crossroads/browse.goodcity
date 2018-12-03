dd43f74c26ef975903a93cc139e24276dca64890
namrataukirde <namrata.ukirde@kiprosh.com>
2018-12-03 14:11:32 +0530

    fix redirection issue

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,26 +1,26 @@
 import applicationController from './application';
 
 export default applicationController.extend({
+  isDetailsComplete(){
+    const user = this.get('session.currentUser');
+    if (!user) { return false; }
+
+    const organisationsUser = user.get('organisationsUsers.firstObject');
+    const organisation = organisationsUser && organisationsUser.get('organisation');
+    const userInfoComplete = user.get('isInfoComplete') && user.hasRole('Charity');
+    const organisationUserComplete = organisationsUser && organisationsUser.get('isInfoComplete');
+
+    return (userInfoComplete && organisation && organisationUserComplete);
+  },
+
   actions: {
     redirectAsPerUserDetails(){
-      if(this.get('isDetailsComplete')){   
-          this.transitionToRoute("request_purpose");
-        }
-        else{
-          this.transitionToRoute("account_details", { queryParams: { bookAppointment: true}});
-        }
+      if(this.isDetailsComplete){   
+        this.transitionToRoute("request_purpose");
+      }
+      else{
+        this.transitionToRoute("account_details", { queryParams: { bookAppointment: true}});
+      }
     },
-    
-    isDetailsComplete(){
-      const user = this.get('session.currentUser');
-      if (!user) { return false; }
-
-      const organisationsUser = user.get('organisationsUsers.firstObject');
-      const organisation = organisationsUser && organisationsUser.get('organisation');
-      const userInfoComplete = user.get('isInfoComplete') && user.hasRole('Charity');
-      const organisationUserComplete = organisationsUser && organisationsUser.get('isInfoComplete');
-
-      return (userInfoComplete && organisation && organisationUserComplete);
-    }
   }
 });
