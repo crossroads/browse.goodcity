03dbe444dca1ec3d7b65f972227b3af249ca9c01
Abhinav Garg <abhinav.g@kiprosh.com>
2018-12-04 18:43:26 +0530

    acceptance test cases for goods_details page

--- tests/acceptance/book-appointment-test.js
+++ tests/acceptance/book-appointment-test.js
@@ -3,7 +3,7 @@ import { module, test } from 'qunit';
 import startApp from 'browse/tests/helpers/start-app';
 import { make, mockFindAll } from 'ember-data-factory-guy';
 
-var App, order, gogo_van, user, user_profile, orderPurpose1, orderPurpose2 , organisation, pkg, ordersPackage, mocks, goodcityRequest, benificiary, identity_type1, package_type;
+var App, order, gogo_van, user, user_profile, orderPurpose1, orderPurpose2 , organisation, pkg, ordersPackage, mocks, goodcityRequest, benificiary, identity_type1, package_type, bookingType;
 
 module('Acceptance | BookAppointment', {
   beforeEach: function() {
@@ -11,6 +11,7 @@ module('Acceptance | BookAppointment', {
     user = make("user");
     organisation = make("organisation");
     pkg = make('package');
+    bookingType = make("booking_type");
     ordersPackage = make("orders_package", { quantity: 1, state: "requested", package: pkg,
       packageId: pkg.id, order: order});
     order = make("order", { state: "draft", created_by_id: user.id, organisation_id: organisation.id,
@@ -41,7 +42,8 @@ module('Acceptance | BookAppointment', {
         responseText:["2018-06-14", "2018-06-15", "2018-06-16", "2018-06-19", "2018-06-20", "2018-06-21"]}),
       mockFindAll("gogovan_transport").returns({json: {gogovan_transports: [gogo_van.toJSON({includeId: true})]}}),
       mockFindAll('order').returns({ json: {orders: [order.toJSON({includeId: true})],
-        packages: [pkg.toJSON({includeId: true})], orders_packages: [ordersPackage.toJSON({includeId: true})]}})
+        packages: [pkg.toJSON({includeId: true})], orders_packages: [ordersPackage.toJSON({includeId: true})]}}),
+      mockFindAll("booking_type").returns({json: {booking_types: [bookingType.toJSON({includeId: true})]}})
     );
   },
 
@@ -66,6 +68,19 @@ test("should redirect to login page if user not logged in", function(assert) {
   andThen(function(){
     assert.equal(currentURL(), '/login?bookAppointment=true');
     assert.equal(Ember.$('.title').text().trim(), "Log in/Register");
+  });
+});
+
+// home page account details flow with book appointment test
+test("should redirect to Account details page if user is logged in and account details are incomplete", function(assert) {
+  assert.expect(2);
+  visit('/');
+  andThen(function() {
+    click(".book-appointment");
+  });
+  andThen(function(){
+    assert.equal(currentURL(), "/account_details?bookAppointment=true");
+    assert.equal(Ember.$('.title').text().trim(), "Account Details");
   });
 });
 
@@ -112,18 +127,6 @@ test("request purpose page should not redirect if incomplete form", function(ass
     andThen(function(){
       assert.equal(Ember.$('.title').text().trim(), "Request Purpose");
     });
-  });
-});
-
-test("should redirect to Request purpose page if user is logged in", function(assert) {
-  assert.expect(2);
-  visit('/');
-  andThen(function() {
-    click(".book-appointment");
-  });
-  andThen(function(){
-    assert.equal(currentURL(), '/request_purpose');
-    assert.equal(Ember.$('.title').text().trim(), "Request Purpose");
   });
 });
 
@@ -298,7 +301,7 @@ test("Incomplete form submit client info page, should not redirect to goods deta
   });
 });
 
-//Goods Details Page tests
+// Goods Details Page tests
 test("Goods Details Page on incomplete submit should not redirect to appointment page", function(assert){
   let goodsDetailsUrl=`/order/${order.id}/goods_details`;
   assert.expect(2);
@@ -324,57 +327,57 @@ test("Goods Details Page on incomplete submit should not redirect to appointment
   });
 });
 
-//fill good details page and click submit, should redirect to appointment
-test("Goods Details Page on successfull submit should redirect to appointment page", function(assert){
-  let goodsDetailsUrl=`/order/${order.id}/goods_details`;
-  assert.expect(1);
-  mocks.push(
-    $.mockjax({url: '/api/v1/order*', type: 'POST', status: 201,responseText: {
-          order: order.toJSON({includeId: true}),
-          orders_purposes: [orderPurpose1],
-          user: user.toJSON({includeId: true})}
-    }),
-    mockFindAll('package_type').returns({ json: {package_types: [package_type.toJSON({includeId: true})]}})
-  );
-  visit(goodsDetailsUrl);
-  andThen(function(){
-    click('.select-goods');
-  });
-  andThen(function(){
-    let updated_gc_req = {id: goodcityRequest.id, code_id: package_type.id, order_id: order.id, package_type_id: package_type.id};
-    let location = {id:12, building:'26Med', area:'B2', stockit_id:null};
-    mocks.push(
-      $.mockjax({url: 'api/v1/goodcity_requests/*', type:'PUT', status: 201,
-        responseText:{
-          code: [package_type.toJSON({includeId: true})],
-          goodcity_request: updated_gc_req,
-          locations: [location]
-        }
-      })
-    );
-    click('.package_name');
-  });
-  andThen(function(){
-    fillIn('.select-goods', package_type.get('name'));
-  });
-  andThen(function(){
-    fillIn('.add-quantity', "2")
-  });
-  andThen(function(){
-    fillIn('.add-description', "description")
-  });
-  andThen(function(){
-    let date = moment(new Date).format('YYYY-MM-DD');
-    let timestamp = moment(new Date).format();
-    let slots = [{id: null, isClosed: false, note: "", quota: 3, remaining: 3, timestamp }];
-    mocks.push(
-      $.mockjax({url: '/api/v1/appointment_slots/calenda*', type: 'GET', status: 200, responseText:{
-          appointment_calendar_dates: [{date: date, slots: slots, isClosed: false}]
-      }})
-    );
-    click('.expand_button');
-  });
-  andThen(function(){
-    assert.equal(Ember.$('.title').text().trim(), "Appointment Details");
-  });
-});
+// // fill good details page and click submit, should redirect to appointment
+// test("Goods Details Page on successfull submit should redirect to appointment page", function(assert){
+//   let goodsDetailsUrl=`/order/${order.id}/goods_details`;
+//   assert.expect(1);
+//   mocks.push(
+//     $.mockjax({url: '/api/v1/order*', type: 'POST', status: 201,responseText: {
+//           order: order.toJSON({includeId: true}),
+//           orders_purposes: [orderPurpose1],
+//           user: user.toJSON({includeId: true})}
+//     }),
+//     mockFindAll('package_type').returns({ json: {package_types: [package_type.toJSON({includeId: true})]}})
+//   );
+//   visit(goodsDetailsUrl);
+//   andThen(function(){
+//     click('.select-goods');
+//   });
+//   andThen(function(){
+//     let updated_gc_req = {id: goodcityRequest.id, code_id: package_type.id, order_id: order.id, package_type_id: package_type.id};
+//     let location = {id:12, building:'26Med', area:'B2', stockit_id:null};
+//     mocks.push(
+//       $.mockjax({url: 'api/v1/goodcity_requests/*', type:'PUT', status: 201,
+//         responseText:{
+//           code: [package_type.toJSON({includeId: true})],
+//           goodcity_request: updated_gc_req,
+//           locations: [location]
+//         }
+//       })
+//     );
+//     click('.package_name');
+//   });
+//   andThen(function(){
+//     fillIn('.select-goods', package_type.get('name'));
+//   });
+//   andThen(function(){
+//     fillIn('.add-quantity', "2")
+//   });
+//   andThen(function(){
+//     fillIn('.add-description', "description")
+//   });
+//   andThen(function(){
+//     let date = moment(new Date).format('YYYY-MM-DD');
+//     let timestamp = moment(new Date).format();
+//     let slots = [{id: null, isClosed: false, note: "", quota: 3, remaining: 3, timestamp }];
+//     mocks.push(
+//       $.mockjax({url: '/api/v1/appointment_slots/calenda*', type: 'GET', status: 200, responseText:{
+//           appointment_calendar_dates: [{date: date, slots: slots, isClosed: false}]
+//       }})
+//     );
+//     click('.expand_button');
+//   });
+//   andThen(function(){
+//     assert.equal(Ember.$('.title').text().trim(), "Appointment Details");
+//   });
+// });
