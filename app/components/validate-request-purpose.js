b681d5f2427a2a5fd14363d8f0ed5d273f166761
Abhinav Garg <abhinav.g@kiprosh.com>
2018-12-19 11:48:34 +0530

    refactor code

--- app/components/validate-request-purpose.js
+++ app/components/validate-request-purpose.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   didInsertElement() {
     this._super();
+    let elementArray = JSON.parse(this.get('elementArray'));
 
     Ember.run.scheduleOnce('afterRender', this, function(){
       validateInputs();
@@ -10,65 +11,43 @@ export default Ember.Component.extend({
     });
 
     function validateInputs(){
-      Ember.$('#people-count').focusout(function(){
-        return checkInput(this);
+      elementArray.forEach(selector => {
+        Ember.$(selector).focusout(function(){
+          return checkInput(this);
+        });
       });
 
-      Ember.$('#people-count').focus(function(){
-        return removeHighlight(this);
+      elementArray.forEach(selector => {
+        Ember.$(selector).focus(function(){
+          return removeHighlight(this);
+        });
       });
-
-      Ember.$('#description').focusout(function(){
-        return checkInput(this);
-      });
-
-      Ember.$('#description').focus(function(){
-        return removeHighlight(this);
-      });
-
-      Ember.$('.district-selector select').focusout(function(){
-        return checkSelect(this);
-      });
-
-      Ember.$('.district-selector select').focus(function(){
-        return removeHighlight(this);
-      });
-    }
-
-    function checkSelect(element){
-      var parent = Ember.$(element).parent();
-      var value = Ember.$(element).val();
-
-      if(value === "") {
-        parent.addClass('form__control--error');
-      } else {
-        parent.removeClass('form__control--error');
-      }
     }
 
     function checkInput(element){
       var parent = Ember.$(element).parent();
       var value = Ember.$(element).val();
 
-      if(value === undefined || value.length === 0) {
+      if(value === undefined || value.length === 0 || value === "") {
         parent.addClass('form__control--error');
       } else {
         parent.removeClass('form__control--error');
       }
     }
 
-    function validateForm(){
-      Ember.$('#request-submit').click(function(){
-        checkInput('#people-count');
-        checkSelect('.district-selector select');
-        checkInput('#description');
-        if(Ember.$('.form__control--error').length > 0) { return false; }
-      });
-    }
-
     function removeHighlight(element){
       var parent = Ember.$(element).parent();
       parent.removeClass('form__control--error');
     }
+
+    function validateForm(){
+      Ember.$('#request-submit').click(function(){
+        elementArray.forEach(selector => {
+          return checkInput(selector);
+        });
+        if(Ember.$('.form__control--error').length > 0) { return false; }
+      });
+    }
+
   }
 });
