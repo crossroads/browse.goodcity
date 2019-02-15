de28aa4f21ffc871d3ff733cc1134213c2689f43
Patrick R <patrixr@Patricks-MacBook-Pro.local>
2019-02-15 14:26:41 +0800

    Update the cart and order properly

--- app/controllers/request_purpose.js
+++ app/controllers/request_purpose.js
@@ -20,6 +20,8 @@ export default Ember.Controller.extend(cancelOrder, {
   selectedId: null,
   isSelfSelected: Ember.computed.equal("selectedId", "organisation"),
   user: Ember.computed.alias('session.currentUser'),
+  cart: Ember.inject.service(),
+  messageBox: Ember.inject.service(),
 
   selectedDistrict: null,
 
@@ -27,8 +29,21 @@ export default Ember.Controller.extend(cancelOrder, {
     return this.store.peekAll('district');
   }),
 
-  getBookingTypeId(identifier) {
+  getBookingTypeIdFor(identifier) {
     return this.store.peekAll('booking_type').filterBy('identifier', identifier).get('firstObject.id');
+  },
+
+  getSelectedBookingTypeId() {
+    let order = this.get('model');
+    let bookingTypeId = order && order.get('bookingTypeId');
+    if (!bookingTypeId) {
+      bookingTypeId = this.getBookingTypeIdFor(this.get('bookAppointment') ? 'appointment' : 'online-order');
+    }
+    return bookingTypeId;
+  },
+
+  isOnlineOrder() {
+    return this.getSelectedBookingTypeId() === this.getBookingTypeIdFor('online-order');
   },
 
   actions: {
@@ -36,7 +51,17 @@ export default Ember.Controller.extend(cancelOrder, {
       this.set("description", "");
     },
 
-    createOrderWithRequestPuropose(){
+    createOrderWithRequestPurpose(){
+      if (this.isOnlineOrder()) {
+        let cartHasItems = this.get("cart.cartItems").length;
+        if(!cartHasItems) {
+          this.get("messageBox").alert(this.get("i18n").t("order.order_detail_pop_up"), () => {
+            this.transitionToRoute("index");
+          });
+          return false;
+        }
+      }
+
       let user = this.get('user');
       let purposeIds = [];
 
@@ -59,19 +84,19 @@ export default Ember.Controller.extend(cancelOrder, {
         actionType = "PUT";
       }
 
-      let bookingTypeId = order && order.get('bookingTypeId');
-      if (!bookingTypeId) {
-        bookingTypeId = this.getBookingTypeId(this.get('bookAppointment') ? 'appointment' : 'online-order');
-      }
-
       let orderParams = {
         organisation_id: user_organisation_id,
         purpose_description: this.get('description'),
         purpose_ids: purposeIds,
         people_helped: this.get('peopleCount'),
         district_id: this.get('selectedDistrict.id'),
-        booking_type_id: bookingTypeId
+        booking_type_id: this.getSelectedBookingTypeId(),
+        state: order ? order.get('state') : 'draft'
       };
+
+      if (this.isOnlineOrder()) {
+        orderParams.cart_package_ids = this.get('cart.packageIds');
+      }
 
       let loadingView = getOwner(this).lookup('component:loading').append();
 
--- app/routes/authorize.js
+++ app/routes/authorize.js
@@ -14,5 +14,6 @@ export default Ember.Route.extend({
       });
       return false;
     }
+    return true;
   }
 });
