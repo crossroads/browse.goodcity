46440b1d5cf2893d159ff0dc60264abf27bb12f0
Patrick R <patrixr@Patricks-MacBook-Pro.local>
2019-02-20 17:40:55 +0800

    Fix test + add test for online-order case

--- tests/acceptance/book-appointment-test.js
+++ tests/acceptance/book-appointment-test.js
@@ -1,17 +1,21 @@
 import Ember from 'ember';
+import _ from 'lodash';
 import { module, test } from 'qunit';
 import startApp from 'browse/tests/helpers/start-app';
 import { make, mockFindAll } from 'ember-data-factory-guy';
 
-var App, order, gogo_van, user, user_profile, orderPurpose1, orderPurpose2 , organisation, pkg, ordersPackage, mocks, goodcityRequest, benificiary, identity_type1, package_type, bookingType, purpose, requestPurposeUrl, goodsDetailsUrl, appointmentPageUrl, clientInfoUrl, bookingSuccessUrl, confirmBookingUrl, orderTransport;
+var App, order, gogo_van, user, user_profile, orderPurpose1, orderPurpose2 , organisation, pkg, ordersPackage, mocks, goodcityRequest, benificiary, identity_type1, package_type, purpose, requestPurposeUrl, goodsDetailsUrl, scheduleDetailsUrl, appointmentPageUrl, clientInfoUrl, bookingSuccessUrl, confirmBookingUrl, orderTransport;
+var BookingTypes = {
+  onlineOrder: { id:1, identifier: 'online-order'},
+  appointment: { id:2, identifier: 'appointment'}
+};
 
-module('Acceptance | BookAppointment', {
+module('Acceptance | BookAppointment - OnlineOrder', {
   beforeEach: function() {
     App = startApp();
     user = make("user");
     organisation = make("organisation");
     pkg = make('package');
-    bookingType = make("booking_type");
     purpose = make("purpose");
     ordersPackage = make("orders_package", { quantity: 1, state: "requested", package: pkg,
       packageId: pkg.id, order: order});
@@ -45,12 +49,21 @@ module('Acceptance | BookAppointment', {
       mockFindAll("gogovan_transport").returns({json: {gogovan_transports: [gogo_van.toJSON({includeId: true})]}}),
       mockFindAll('order').returns({ json: {orders: [order.toJSON({includeId: true})],
         packages: [pkg.toJSON({includeId: true})], orders_packages: [ordersPackage.toJSON({includeId: true})]}}),
-      mockFindAll("booking_type").returns({json: {booking_types: [bookingType.toJSON({includeId: true})]}}),
+      mockFindAll("booking_type").returns({json: {booking_types: _.values(BookingTypes)}}),
       mockFindAll("purpose").returns({json: {booking_types: [purpose.toJSON({includeId: true})]}})
+    );
+    let date = moment(new Date()).format('YYYY-MM-DD'); // jshint ignore:line
+    let timestamp = moment(new Date()).format(); // jshint ignore:line
+    let slots = [{id: null, isClosed: false, note: "", quota: 3, remaining: 3, timestamp }];
+    mocks.push(
+      $.mockjax({url: '/api/v1/appointment_slots/calenda*', type: 'GET', status: 200, responseText:{
+          appointment_calendar_dates: [{date: date, slots: slots, isClosed: false}]
+      }})
     );
     requestPurposeUrl = '/request_purpose';
     clientInfoUrl = `/order/${order.id}/client_information`;
     goodsDetailsUrl = `/order/${order.id}/goods_details`;
+    scheduleDetailsUrl = `/order/${order.id}/schedule_details`;
     appointmentPageUrl = `/order/${order.id}/schedule_details`;
     confirmBookingUrl = `/order/${order.id}/confirm_booking`;
     bookingSuccessUrl = `/order/${order.id}/booking_success`;
@@ -228,12 +241,79 @@ test("Select RBCL on client info should display form for rbcl", function(assert)
   });
 });
 
-test("Filled Up client info page, should redirect to goods details page on submit", function(assert){
-  orderPurpose2 = {id: 1, purposes: [{id: 2}], order_id: order.id, designation_id: 1};
+test("Online order : Filled Up client info page, should redirect to schedule details page on submit", function(assert){
+  orderPurpose2 = {id: 1, purposes: [{id: 2}], order_id: order.id, designation_id: 1, booking_type_id: BookingTypes.onlineOrder.id};
   assert.expect(2);
+
   mocks.push(
     $.mockjax({url: '/api/v1/order*', type: 'GET', status: 201,responseText: {
         order: order.toJSON({includeId: true}),
+        orders_purposes: [orderPurpose2],
+        user: user.toJSON({includeId: true}),
+        goodcity_requests: []
+      }
+    })
+  );
+  visit('/');
+  andThen(function(){
+    visit(clientInfoUrl);
+  });
+  andThen(function(){
+    click('.custom_radio_buttons .for-client');
+  });
+  andThen(function(){
+    fillIn("#hk-id-number", "1234");
+  });
+  andThen(function(){
+    fillIn("#hk-id-firstName", "Test");
+  });
+  andThen(function(){
+    fillIn("#hk-id-lastName", "John");
+  });
+  andThen(function(){
+    fillIn("#mobile", "98876353");
+  });
+  andThen(function(){
+    mocks.push(
+      $.mockjax({url: '/api/v1/order*', type: 'PUT', status: 201,responseText: {
+        order: order.toJSON({includeId: true}),
+        orders_purposes: [orderPurpose2],
+        user: user.toJSON({includeId: true})}
+      }),
+      $.mockjax({url: '/api/v1/beneficiarie*', type: 'POST', status: 201,
+        responseText: {
+          beneficiary: benificiary,
+          identity_types: [identity_type1]
+        }
+      })
+    );
+    click('#client-info-submit');
+    andThen(function(){
+      let updated_gc_req = {id: goodcityRequest.id, code_id: package_type.id, order_id: order.id, package_type_id: package_type.id};
+      let location = {id:12, building:'26Med', area:'B2', stockit_id:null};
+      mocks.push(
+        $.mockjax({url: 'api/v1/goodcity_requests/*', type:'PUT', status: 201,
+          responseText:{
+            code: [package_type.toJSON({includeId: true})],
+            goodcity_request: updated_gc_req,
+            locations: [location]
+          }
+        })
+      );
+      assert.equal(currentURL(), scheduleDetailsUrl);
+      assert.equal(Ember.$('.title').text().trim(), "Transport Details");
+    });
+  });
+});
+
+test("Appointment : Filled Up client info page, should redirect to goods details page on submit", function(assert){
+  orderPurpose2 = {id: 1, purposes: [{id: 2}], order_id: order.id, designation_id: 1 };
+  assert.expect(2);
+
+  let appointment = _.extend({}, order.toJSON({includeId: true}), { booking_type_id: BookingTypes.appointment.id });
+  mocks.push(
+    $.mockjax({url: '/api/v1/order*', type: 'GET', status: 201,responseText: {
+        order: appointment,
         orders_purposes: [orderPurpose2],
         user: user.toJSON({includeId: true}),
         goodcity_requests: []
