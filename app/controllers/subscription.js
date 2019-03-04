09eccfe9632247eee69911d424afbf277e9b44f2
Abhinav Garg <abhinav.g@kiprosh.com>
2019-03-04 19:25:26 +0530

    break the logic into methods

--- app/controllers/subscription.js
+++ app/controllers/subscription.js
@@ -123,6 +123,33 @@ export default Ember.Controller.extend({
     this.get("store").findAll("package");
   },
 
+  generateMessageUrl: function(data, type){
+    var router = this.get("target");
+    var messageRoute = this.get("messagesUtil").getRoute(data.item[type]);
+    var messageUrl = router.generate.apply(router, messageRoute);
+    return messageUrl.split("#").get("lastObject");
+  },
+
+  markReadAndScroll: function(message){
+    if (message && !message.get("isRead")) {
+      this.get("messagesUtil").markRead(message);
+
+      var scrollOffset;
+      if (Ember.$(".message-textbar").length > 0) {
+        scrollOffset = Ember.$(document).height();
+      }
+
+      var screenHeight = document.documentElement.clientHeight;
+      var pageHeight = document.documentElement.scrollHeight;
+
+      if (scrollOffset && pageHeight > screenHeight) {
+        Ember.run.later(this, function () {
+          window.scrollTo(0, scrollOffset);
+        });
+      }
+    }
+  },
+
   // each action below is an event in a channel
   update_store: function(data, success) {
     if(data["item"]["designation"]) {
@@ -207,33 +234,12 @@ export default Ember.Controller.extend({
 
     // mark message read here
     if (type === "message") {
-      var router = this.get("target");
+      var messageUrl = this.generateMessageUrl(data, type);
       var currentUrl = window.location.href.split("#").get("lastObject");
-
-      var messageRoute = this.get("messagesUtil").getRoute(data.item[type]);
-      var messageUrl = router.generate.apply(router, messageRoute);
-      messageUrl = messageUrl.split("#").get("lastObject");
-
 
       if (currentUrl.indexOf(messageUrl) >= 0) {
         var message = this.store.peekRecord("message", item.id);
-        if (message && !message.get("isRead")) {
-          this.get("messagesUtil").markRead(message);
-
-          var scrollOffset;
-          if (Ember.$(".message-textbar").length > 0) {
-            scrollOffset = Ember.$(document).height();
-          }
-
-          var screenHeight = document.documentElement.clientHeight;
-          var pageHeight = document.documentElement.scrollHeight;
-
-          if (scrollOffset && pageHeight > screenHeight) {
-            Ember.run.later(this, function () {
-              window.scrollTo(0, scrollOffset);
-            });
-          }
-        }
+        this.markReadAndScroll(message);
       }
     }
 
