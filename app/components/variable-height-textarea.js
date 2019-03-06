5540fdf875da6b01da396ff3ee52ef9880c6caf4
Abhinav Garg <abhinav.g@kiprosh.com>
2019-03-06 15:41:14 +0530

    restrict blank message

--- app/components/variable-height-textarea.js
+++ app/components/variable-height-textarea.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.TextArea.extend({
   tagName: "textarea",
   attributeBindings: ["disabled"],
+  classNames: 'message_textbar',
   disabled: false,
 
    didDestroyElement: function () {
@@ -47,6 +48,7 @@ export default Ember.TextArea.extend({
       .height(textarea.scrollHeight - 15);
     // scroll to bottom if message typed
     if (_this.get('value') !== "") {
+      Ember.$('.message_textbar').parent().removeClass('has-error');
       _this.autoScroll();
     }
   },
