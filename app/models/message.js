97125066b0019d5dffed69ee621e492dac00707c
Abhinav Garg <abhinav.g@kiprosh.com>
2019-03-11 15:11:56 +0530

    fix new user issues

--- app/models/message.js
+++ app/models/message.js
@@ -1,38 +1,38 @@
-import Ember from 'ember';
-import DS from 'ember-data';
+import Ember from "ember";
+import DS from "ember-data";
 
 var attr = DS.attr,
   belongsTo = DS.belongsTo;
 
 export default DS.Model.extend({
   session: Ember.inject.service(),
-  body: attr('string'),
-  senderId: attr('number'),
-  isPrivate: attr('boolean'),
-  createdAt: attr('date'),
-  updatedAt: attr('date'),
-  state: attr('string', {
-    defaultValue: 'read'
+  body: attr("string"),
+  senderId: attr("number"),
+  isPrivate: attr("boolean"),
+  createdAt: attr("date"),
+  updatedAt: attr("date"),
+  state: attr("string", {
+    defaultValue: "read"
   }),
-  sender: belongsTo('sender', {
+  sender: belongsTo("user", {
     async: false
   }),
-  order: belongsTo('order', {
+  order: belongsTo("order", {
     async: false
   }),
 
-  myMessage: Ember.computed(function () {
-    return this.get("sender.id") === this.get('session.currentUser.id');
+  myMessage: Ember.computed(function() {
+    return this.get("sender.id") === this.get("session.currentUser.id");
   }),
 
-  isMessage: Ember.computed('this', function () {
+  isMessage: Ember.computed("this", function() {
     return true;
   }),
 
-  createdDate: Ember.computed(function () {
+  createdDate: Ember.computed(function() {
     return new Date(this.get("createdAt")).toDateString();
   }),
 
-  isRead: Ember.computed.equal('state', 'read'),
-  isUnread: Ember.computed.equal('state', 'unread')
+  isRead: Ember.computed.equal("state", "read"),
+  isUnread: Ember.computed.equal("state", "unread")
 });
