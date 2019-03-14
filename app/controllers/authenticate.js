d07c4ef2d246e7505873f71586b22e1ca89db33f
namrataukirde <namrata.ukirde@kiprosh.com>
2019-03-14 19:03:15 +0530

    change login field to accept both email and mobile number

--- app/controllers/authenticate.js
+++ app/controllers/authenticate.js
@@ -1,46 +1,62 @@
-import Ember from 'ember';
-import AjaxPromise from './../utils/ajax-promise';
-import config from '../config/environment';
+import Ember from "ember";
+import AjaxPromise from "./../utils/ajax-promise";
+import config from "../config/environment";
 const { getOwner } = Ember;
 
 export default Ember.Controller.extend({
-  queryParams: ['bookAppointment'],
+  queryParams: ["bookAppointment"],
   messageBox: Ember.inject.service(),
   application: Ember.inject.controller(),
   attemptedTransition: null,
   bookAppointment: false,
   pin: "",
-  mobilePhone: "",
+  mobileOrEmail: "",
+  mobile: "",
+  email: "",
 
-  mobile: Ember.computed('mobilePhone', function(){
-    return config.APP.HK_COUNTRY_CODE + this.get('mobilePhone');
-  }),
+  setMobileOrEmail() {
+    if (/\+852[569]\d{7}/.test(this.get("mobileOrEmail"))) {
+      this.set("mobile", this.get("mobileOrEmail"));
+    } else {
+      this.set("email", this.get("mobileOrEmail"));
+    }
+  },
 
   actions: {
-
     authenticateUser(bookAppointment) {
-      Ember.$('.auth_error').hide();
-      var pin = this.get('pin');
-      var otp_auth_key = this.get('session.otpAuthKey');
+      Ember.$(".auth_error").hide();
+      var pin = this.get("pin");
+      var otp_auth_key = this.get("session.otpAuthKey");
       var _this = this;
 
-      var loadingView = getOwner(this).lookup('component:loading').append();
-      new AjaxPromise("/auth/verify", "POST", null, {pin: pin, otp_auth_key: otp_auth_key})
+      var loadingView = getOwner(this)
+        .lookup("component:loading")
+        .append();
+      new AjaxPromise("/auth/verify", "POST", null, {
+        pin: pin,
+        otp_auth_key: otp_auth_key
+      })
         .then(function(data) {
-          _this.setProperties({pin:null});
-          _this.set('session.authToken', data.jwt_token);
-          _this.set('session.otpAuthKey', null);
+          _this.setProperties({ pin: null });
+          _this.set("session.authToken", data.jwt_token);
+          _this.set("session.otpAuthKey", null);
           _this.store.pushPayload(data.user);
-          _this.setProperties({pin: null});
-          _this.get("application").set('loggedInUser', true);
-          _this.transitionToRoute('post_login' , {
+          _this.setProperties({ pin: null });
+          _this.get("application").set("loggedInUser", true);
+          _this.transitionToRoute("post_login", {
             queryParams: { bookAppointment: bookAppointment }
           });
         })
         .catch(function(jqXHR) {
-          Ember.$('#pin').closest('div').addClass('error');
-          _this.setProperties({pin: null});
-          if (jqXHR.status === 422 && jqXHR.responseJSON.errors && jqXHR.responseJSON.errors.pin) {
+          Ember.$("#pin")
+            .closest("div")
+            .addClass("error");
+          _this.setProperties({ pin: null });
+          if (
+            jqXHR.status === 422 &&
+            jqXHR.responseJSON.errors &&
+            jqXHR.responseJSON.errors.pin
+          ) {
             _this.get("messageBox").alert(jqXHR.responseJSON.errors.pin);
           }
           console.log("Unable to authenticate");
@@ -49,27 +65,37 @@ export default Ember.Controller.extend({
     },
 
     resendPin() {
-      var mobile = this.get('mobile');
-      var loadingView = getOwner(this).lookup('component:loading').append();
+      this.setMobileOrEmail();
+      var mobile = this.get("mobile");
+      var email = this.get("email");
+      var loadingView = getOwner(this)
+        .lookup("component:loading")
+        .append();
       var _this = this;
-      var user_auth = { mobile: mobile,
-                        address_attributes: {
-                          district_id: null,
-                          address_type: null
-                        }
-                      };
+      var user_auth = {
+        mobile: mobile,
+        email: email,
+        address_attributes: {
+          district_id: null,
+          address_type: null
+        }
+      };
 
-      new AjaxPromise("/auth/signup", "POST", null, {user_auth: user_auth})
+      new AjaxPromise("/auth/signup", "POST", null, { user_auth: user_auth })
         .then(data => {
-          this.set('session.otpAuthKey', data.otp_auth_key);
-          this.setProperties({pin:null});
-          this.transitionToRoute('authenticate', { queryParams: { bookAppointment: this.get('bookAppointment') }});
+          this.set("session.otpAuthKey", data.otp_auth_key);
+          this.setProperties({ pin: null });
+          this.transitionToRoute("authenticate", {
+            queryParams: { bookAppointment: this.get("bookAppointment") }
+          });
         })
         .catch(error => {
-          if([401].indexOf(error.status) >= 0) {
-             _this.get("messageBox").alert(this.get('i18n').t('unauthorized'), () => {
-              _this.transitionToRoute("/");
-             });
+          if ([401].indexOf(error.status) >= 0) {
+            _this
+              .get("messageBox")
+              .alert(this.get("i18n").t("unauthorized"), () => {
+                _this.transitionToRoute("/");
+              });
           } else if ([422, 403].indexOf(error.status) >= 0) {
             _this.get("messageBox").alert(error.responseJSON.errors);
           }
