4f494e5c926f309f67a87920d57f965dd2cea88f
AbulAsar <abul.a@kiprosh.com>
2019-03-29 16:50:45 +0530

    account details UI fix | scrolling to top on schedule and confirm booking

--- app/routes/order/confirm_booking.js
+++ app/routes/order/confirm_booking.js
@@ -1,17 +1,24 @@
-import AuthorizeRoute from './../authorize';
+import AuthorizeRoute from "./../authorize";
 
 export default AuthorizeRoute.extend({
-	model() {
-    var orderId = this.paramsFor('order').order_id;
-    return this.store.peekRecord('order', orderId) || this.store.findRecord('order', orderId);
+  model() {
+    var orderId = this.paramsFor("order").order_id;
+    return (
+      this.store.peekRecord("order", orderId) ||
+      this.store.findRecord("order", orderId)
+    );
+  },
+
+  afterModel() {
+    window.scrollTo(0, 0); //https://github.com/dollarshaveclub/ember-router-scroll. Read this link for nested route issue for not scrolling at top of the page
   },
 
   setupController() {
     this._super(...arguments);
-    this.controllerFor('application').set('showSidebar', false);
+    this.controllerFor("application").set("showSidebar", false);
   },
 
-  deactivate(){
-    this.controllerFor('application').set('showSidebar', true);
+  deactivate() {
+    this.controllerFor("application").set("showSidebar", true);
   }
 });
