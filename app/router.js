97c3460fe1bf3df58a8d41d375847e6032a18029
Abhinav Garg <abhinav.g@kiprosh.com>
2019-04-03 17:22:58 +0530

    add update and account page

--- app/router.js
+++ app/router.js
@@ -1,5 +1,5 @@
-import Ember from 'ember';
-import config from './config/environment';
+import Ember from "ember";
+import config from "./config/environment";
 
 const Router = Ember.Router.extend({
   location: config.locationType,
@@ -9,45 +9,47 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route("home");
   this.route("browse");
-  this.route('package_category', { path: '/category/:id' });
-  this.route('item', { path: '/item/:id' });
-  this.route('package', { path: '/package/:id' });
-  this.route('cart');
+  this.route("package_category", { path: "/category/:id" });
+  this.route("item", { path: "/item/:id" });
+  this.route("package", { path: "/package/:id" });
+  this.route("cart");
 
-  this.route('login');
-  this.route('account_details');
-  this.route('post_login');
-  this.route('authenticate');
-  this.route('search_organisation');
+  this.route("login");
+  this.route("account_details");
+  this.route("post_login");
+  this.route("authenticate");
+  this.route("search_organisation");
 
-  this.route('my_orders');
+  this.route("my_orders");
+  this.route("my_account");
+  this.route("my_updates");
 
-  this.route('orders', function(){
-    this.route('detail', { path: '/:order_id'});
-    this.route('booking', { path: '/:order_id/booking'});
-    this.route('goods', { path: '/:order_id/goods'});
-    this.route('conversation', {
-      path: '/:order_id/conversation'
+  this.route("orders", function() {
+    this.route("detail", { path: "/:order_id" });
+    this.route("booking", { path: "/:order_id/booking" });
+    this.route("goods", { path: "/:order_id/goods" });
+    this.route("conversation", {
+      path: "/:order_id/conversation"
     });
   });
 
-  this.route('order', { path: '/order/:order_id/' }, function() {
-    this.route('confirm');
-    this.route('client_information');
-    this.route('goods_details');
+  this.route("order", { path: "/order/:order_id/" }, function() {
+    this.route("confirm");
+    this.route("client_information");
+    this.route("goods_details");
     this.route("search_code");
-    this.route('schedule_details');
-    this.route('confirm_booking');
-    this.route('booking_success');
+    this.route("schedule_details");
+    this.route("confirm_booking");
+    this.route("booking_success");
   });
 
-  this.route('confirm');
-  this.route('not-found', { path: '/*path' });
-  this.route('privacy');
-  this.route('faq');
-  this.route('terms');
-  this.route('about');
-  this.route('request_purpose');
+  this.route("confirm");
+  this.route("not-found", { path: "/*path" });
+  this.route("privacy");
+  this.route("faq");
+  this.route("terms");
+  this.route("about");
+  this.route("request_purpose");
 });
 
 export default Router;
