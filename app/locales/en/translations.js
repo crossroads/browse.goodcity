39c3658c86af2a35fb2543f769def1aef4bf2e6b
Abhinav Garg <abhinav.g@kiprosh.com>
2019-04-18 16:43:03 +0530

    fix post email input screen

--- app/locales/en/translations.js
+++ app/locales/en/translations.js
@@ -223,7 +223,7 @@ export default {
   },
 
   _verification_pin: {
-    input_code: "Input 4-digit SMS code",
+    input_code: "Input 4-digit code",
     send_code: "We just send a code to",
     auth_error: "Sorry! Please enter the correct pin."
   },
