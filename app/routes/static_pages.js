99e50dc818a2e7c281b437bf8a464b23e6277a0e
Abhinav Garg <abhinav.g@kiprosh.com>
2019-05-07 16:52:02 +0530

    handle back routing and show image to small screens too

--- app/routes/static_pages.js
+++ app/routes/static_pages.js
@@ -1,10 +1,24 @@
 import Ember from "ember";
 
 export default Ember.Route.extend({
+  previousRouteName: null,
+  backLink: null,
+  previousURL: null,
+
+  beforeModel() {
+    this.set("previousRouteName", null);
+    var previousRoutes = this.router.router.currentHandlerInfos;
+    var previousRoute = previousRoutes && previousRoutes.pop();
+    if (previousRoute && this.isPreviousRouteStatic(previousRoute.name)) {
+      this.set("previousRouteName", "home");
+    }
+    this.set("previousURL", this.get("router").currentURL);
+  },
+
   setupController(controller, model, transition) {
     this._super(...arguments);
     let applicationController = this.controllerFor("application");
-    applicationController.set("showSidebar", true);
+    applicationController.set("hideHeaderBar", true);
     applicationController.set(
       "pageTitle",
       this.get("i18n").t(`${transition.targetName}.title`)
@@ -14,10 +28,27 @@ export default Ember.Route.extend({
   resetController: function(controller, isExiting) {
     this._super.apply(this, arguments);
     if (isExiting) {
-      this.controllerFor("application").set(
+      let applicationController = this.controllerFor("application");
+      applicationController.set(
         "pageTitle",
         this.get("i18n").t("browse.title")
       );
+      applicationController.set("hideHeaderBar", false);
+    }
+  },
+
+  isPreviousRouteStatic(previousPathName) {
+    return ["about", "terms", "faq", "privacy"].indexOf(previousPathName) >= 0;
+  },
+
+  actions: {
+    back: function() {
+      let prevPathName = this.get("previousRouteName");
+      if (prevPathName) {
+        this.transitionTo(prevPathName);
+      } else {
+        this.transitionTo(this.get("previousURL"));
+      }
     }
   }
 });
