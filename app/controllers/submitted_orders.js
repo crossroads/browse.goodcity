2b77c5db75487e331ba950dfcf0aebb5d3526506
AbulAsar <abul.a@kiprosh.com>
2019-07-12 20:18:11 +0530

    after merging packages to existing order moved to orders.goods page along with warning messsage.

--- app/controllers/submitted_orders.js
+++ app/controllers/submitted_orders.js
@@ -1,19 +1,51 @@
 import Ember from "ember";
+import asyncTasksMixin from "../mixins/async_tasks";
+import { task } from "ember-concurrency";
+import _ from "lodash";
 
-export default Ember.Controller.extend({
-  orderId: "new_order",
+export default Ember.Controller.extend(asyncTasksMixin, {
+  cart: Ember.inject.service(),
+  orderService: Ember.inject.service(),
+  orderId: "",
+
+  badCart() {
+    return !this.get("cart.canCheckout");
+  },
+
+  emptyCart() {
+    return this.get("cart.isEmpty");
+  },
+
+  checkoutCart: task(function*() {
+    const orderId = this.get("orderId");
+    const order = yield this.store.peekRecord("order", orderId);
+    if (this.emptyCart()) {
+      return this.i18nAlert("cart_content.empty_cart", _.noop);
+    }
+    if (this.badCart()) {
+      return this.i18nAlert("items_not_available", _.noop);
+    }
+    try {
+      yield this.runTask(this.get("cart").checkoutOrder(order));
+    } catch (error) {
+      throw error;
+    }
+    this.transitionToRoute("orders.goods", orderId);
+  }),
 
   actions: {
     mergePackage() {
-      const orderId =
-        this.get("orderId") === "new_order" ? null : this.get("orderId");
-      this.transitionToRoute("request_purpose", {
-        queryParams: {
-          onlineOrder: true,
-          bookAppointment: false,
-          orderId: orderId
-        }
-      });
+      if (this.get("orderId")) {
+        this.get("checkoutCart").perform();
+      } else {
+        this.transitionToRoute("request_purpose", {
+          queryParams: {
+            onlineOrder: true,
+            bookAppointment: false,
+            orderId: null
+          }
+        });
+      }
     }
   }
 });
