a6a522673a0295626a7210f85620952df7d6e2e9
shreyas098 <shreyash@kiprosh.com>
2020-08-20 13:52:10 +0530

    Suggestion for the PR added

--- app/routes/orders/booking.js
+++ app/routes/orders/booking.js
@@ -2,7 +2,9 @@ import detail from "./detail";
 
 export default detail.extend({
   afterModel() {
-    window.scrollTo(0, 0); //https://github.com/dollarshaveclub/ember-router-scroll. Read this link for nested route issue for not scrolling at top of the page
+    // https://github.com/dollarshaveclub/ember-router-scroll.
+    // Read this link for nested route issue for not scrolling at top of the page
+    window.scrollTo(0, 0);
   },
 
   setupController(controller, model) {
