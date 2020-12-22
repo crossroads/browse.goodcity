6e641d90688a8a22e057a379b3721ebb43d32cca
AbulAsar Sayyad <abul.a@kiprosh.com>
2020-12-22 10:45:57 +0530

    fixed: navigation between offer details and chat

---
+++ app/controllers/offers/messages.js
@@ -0,0 +1,48 @@
+import Controller from "@ember/controller";
+import { sort } from "@ember/object/computed";
+
+export default Controller.extend({
+  queryParams: ["uid"],
+  sortProperties: ["createdAt: asc"],
+  sortedMessages: sort("messages", "sortProperties"),
+  body: "",
+
+  createMessage(values) {
+    if (values.body) {
+      var message = this.store.createRecord("message", values);
+      message
+        .save()
+        .then(() => {
+          this.set("body", "");
+        })
+        .catch(error => {
+          this.store.unloadRecord(message);
+          throw error;
+        });
+    }
+  },
+
+  actions: {
+    sendMessage() {
+      $("textarea").trigger("blur");
+      var values = this.getProperties("body");
+      values.body = values.body.trim();
+      // values.order = this.get("model");
+      values.body = values.body.trim();
+
+      values.body = Ember.Handlebars.Utils.escapeExpression(values.body || "");
+      values.body = values.body.replace(/(\r\n|\n|\r)/gm, "<br>");
+      values.isPrivate = false;
+      values.createdAt = new Date();
+      values.messageableType = "Offer";
+      values.messageableId = this.get("model.id");
+      values.sender = this.store.peekRecord(
+        "user",
+        this.get("session.currentUser.id")
+      );
+      console.log(this.get("uid"));
+
+      this.createMessage(values);
+    }
+  }
+});
