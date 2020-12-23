957bb15c7040eea85b5b49bf258154bb85826daa
AbulAsar Sayyad <abul.a@kiprosh.com>
2020-12-23 16:11:50 +0530

    sorting added for offers list page

---
+++ app/controllers/offers/index.js
@@ -0,0 +1,37 @@
+import Controller from "@ember/controller";
+import { computed } from "@ember/object";
+import _ from "lodash";
+
+export default Controller.extend({
+  showSearch: false,
+
+  selectedSort: computed({
+    get() {
+      return this.get("sortOptions.firstObject.id");
+    },
+    set(key, value) {
+      return value.id || value;
+    }
+  }),
+
+  offers: computed("model", "selectedSort", function() {
+    const sortedOffer = _.sortBy(this.get("model"), [
+      function(offer) {
+        return offer.created_at;
+      }
+    ]);
+    return this.get("selectedSort") == "desc"
+      ? sortedOffer
+      : sortedOffer.reverse();
+  }),
+
+  sortOptions: computed(function() {
+    return [
+      {
+        name: this.get("i18n").t("category.sort.newfirst"),
+        id: "desc"
+      },
+      { name: this.get("i18n").t("category.sort.oldfirst"), id: "asc" }
+    ];
+  })
+});
