5acb70f059003bd630f47ab822c29f2148f96ada
bharat619 <bharat.h@kiprosh.com>
2021-03-18 10:03:52 +0530

    :lipstick: UI design improvements - Handle errors from API

--- app/components/change-phone-number-overlay.js
+++ app/components/change-phone-number-overlay.js
@@ -1,10 +1,12 @@
 import Component from "@ember/component";
 import { inject as service } from "@ember/service";
 import { computed } from "@ember/object";
+import AsyncMixin from "browse/mixins/async_tasks";
 
-export default Component.extend({
+export default Component.extend(AsyncMixin, {
   accountService: service(),
   store: service(),
+  messageBox: service(),
 
   mobileNumber: "",
 
@@ -22,28 +24,37 @@ export default Component.extend({
     },
 
     async initiateChange() {
-      const data = await this.get("accountService").initiatePinFor(
-        this.get("formattedMobileNumber"),
-        this.get("session.currentUser.id")
-      );
-      this.set("otpAuth", data.otp_auth_key);
-      this.set("showPinInput", true);
+      try {
+        const data = await this.runTask(
+          this.get("accountService").initiatePinFor(
+            this.get("formattedMobileNumber"),
+            this.get("session.currentUser.id")
+          )
+        );
+        this.set("otpAuth", data.otp_auth_key);
+        this.set("showPinInput", true);
+      } catch (error) {
+        this.get("messageBox").alert(error.responseJSON.error);
+      }
     },
 
-    updatePhoneNumber() {
-      this.get("accountService")
-        .updatePhoneNumber(this.get("session.currentUser.id"), {
-          mobile: this.get("formattedMobileNumber"),
-          otp_auth_key: this.get("otpAuth"),
-          pin: this.get("pin")
-        })
-        .then(response => {
-          this.get("store").pushPayload(response.user);
-          this.send("cancelSearch");
-        })
-        .catch(err => {
-          console.log(err);
-        });
+    async updatePhoneNumber() {
+      try {
+        const data = await this.runTask(
+          this.get("accountService").updatePhoneNumber(
+            this.get("session.currentUser.id"),
+            {
+              mobile: this.get("formattedMobileNumber"),
+              otp_auth_key: this.get("otpAuth"),
+              pin: this.get("pin")
+            }
+          )
+        );
+        this.get("model").set("mobile", data.user.mobile);
+        this.send("cancelSearch");
+      } catch (error) {
+        this.get("messageBox").alert(error.responseJSON.errors[0].message.pin);
+      }
     }
   }
 });
