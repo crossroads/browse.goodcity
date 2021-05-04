49531808f19ce0cc546c88fe1ebbd3f998a80f2c
saunmanoj888 <manoj@kiprosh.com>
2021-05-04 16:45:28 +0530

    GCW-3341 - Fix issue while adding set to cart that makes the site non-functional

--- app/controllers/package.js
+++ app/controllers/package.js
@@ -1,6 +1,12 @@
 import { alias } from "@ember/object/computed";
 import packageSetController from "./package_set";
+import { computed } from "@ember/object";
 
 export default packageSetController.extend({
-  package: alias("model")
+  package: alias("model"),
+
+  isPackagePartOfSet: computed("package", function() {
+    let packageSet = this.get("package.packageSet");
+    return packageSet && packageSet.get("packages").length > 1;
+  })
 });
