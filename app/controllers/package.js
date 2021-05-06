c4a5f26a85945512fc0cc7c5836942f5ff065358
saunmanoj888 <manoj@kiprosh.com>
2021-05-06 17:28:14 +0530

    GCW-3341 Disable "add item" button for individual item of set when set has more than 1 items

--- app/controllers/package.js
+++ app/controllers/package.js
@@ -5,8 +5,12 @@ import { computed } from "@ember/object";
 export default packageSetController.extend({
   package: alias("model"),
 
-  isPackagePartOfSet: computed("package", function() {
-    let packageSet = this.get("package.packageSet");
-    return packageSet && packageSet.get("packages").length > 1;
-  })
+  isPackagePartOfSet: computed(
+    "package",
+    "package.packageSet.packages.[]",
+    function() {
+      let packageSet = this.get("package.packageSet");
+      return packageSet && packageSet.get("packages").length > 1;
+    }
+  )
 });
