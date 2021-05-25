1120d98ff71e1d629ee4087f7aeb3efab35a3f23
shreyas098 <shreyash@kiprosh.com>
2021-05-26 00:57:50 +0530

    :bug: GCW-3341 Fix packageSet errors in cart while unpublishing packages

--- app/controllers/package_set.js
+++ app/controllers/package_set.js
@@ -29,6 +29,14 @@ export default Controller.extend({
   canRedirectToStock: computed.alias("session.currentUser.canRedirectToStock"),
 
   presentInCart: computed("model", "cart.counter", function() {
+    if (this.get("model.isSet")) {
+      let partOfSetPresentInCart = this.get("model.packages")
+        .map(pkg => {
+          return this.get("cart").contains(pkg);
+        })
+        .some(value => value);
+      return !!partOfSetPresentInCart;
+    }
     return this.get("cart").contains(this.get("model"));
   }),
 
@@ -47,14 +55,19 @@ export default Controller.extend({
   ),
   packageUnavailableInSet: computed(
     "model",
+    "model.isSet",
+    "allPackages.@each.isAvailable",
     "allPackages.@each.availableQuantity",
     function() {
       if (!this.get("model")) {
         return true;
       }
-      let quantity = this.get("allPackages").any(
-        pkg => pkg.get("availableQuantity") == 0
-      );
+      let quantity = this.get("allPackages").any(pkg => {
+        return (
+          pkg.get("availableQuantity") == 0 ||
+          pkg.get("allowWebPublish") == false
+        );
+      });
       return !!quantity;
     }
   ),
