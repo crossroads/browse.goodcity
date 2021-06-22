bade28021807112565842f3913b1ce6cea7dca91
swatijadhav <swati.nakshtra17@gmail.com>
2021-06-22 15:30:17 +0530

    GCW-3662 Launch account-details page for incomplete user-profile when responding to offer-messages

--- app/routes/offers/messages.js
+++ app/routes/offers/messages.js
@@ -1,6 +1,16 @@
 import AuthorizeRoute from "../authorize";
 
 export default AuthorizeRoute.extend({
+  beforeModel(transition) {
+    this._super(...arguments);
+
+    if (!this.get("session").accountDetailsComplete()) {
+      transition.abort();
+      this.controllerFor("login").set("attemptedTransition", transition);
+      this.get("router").transitionTo("account_details");
+    }
+  },
+
   model({ offer_id }) {
     this.set("offerId", offer_id);
     return this.store.query("message", {
