d3acd638666f38934b364d5f1d85e72456440f54
shreyas098 <shreyash@kiprosh.com>
2021-08-17 19:23:16 +0530

    :zap: Add changes for shareable offer feature and addressing comments on ticket 3664

--- app/routes/offers/detail.js
+++ app/routes/offers/detail.js
@@ -4,35 +4,39 @@ import _ from "lodash";
 
 export default PublicRoute.extend({
   offerService: service(),
+  session: service(),
 
   async model({ offer_id }) {
     this.set("offerResponsePresent", false);
-    let offerShareable = await this.get("offerService").getDetailOffer(
-      offer_id
-    );
-    this.set("offerShareable", offerShareable);
-    let offerResponse = await this.store.query("offerResponse", {
-      offer_response: {
-        user_id: this.get("session.currentUser").id,
-        offer_id: offerShareable.id
+    this.set("offerNotPresent", false);
+    let offerShareable = await this.get("offerService")
+      .getDetailOffer(offer_id)
+      .catch(e => {
+        return false;
+      });
+
+    if (offerShareable && this.session.get("isLoggedIn")) {
+      let offerResponse = await this.store.query("offerResponse", {
+        offer_response: {
+          user_id: this.get("session.currentUser").id,
+          offer_id: offerShareable.id
+        }
+      });
+      if (offerResponse.content.length > 0) {
+        this.set("offerResponsePresent", true);
       }
-    });
-    if (offerResponse.content.length > 0) {
-      this.set("offerResponsePresent", true);
     }
-    return offerShareable;
+    return offerShareable ? offerShareable : this.set("offerNotPresent", true);
   },
 
   setupController(controller, model) {
-    let offerNotPresent = false;
-    let expiresAt = model.expires_at;
-    if (!this.session.get("isLoggedIn")) {
-      offerNotPresent = true;
-    }
-    if (!this.get("offerNotPresent") && expiresAt) {
-      offerNotPresent = moment(expiresAt) < moment();
+    let expiresAt = model && model.expires_at;
+    if (expiresAt && moment(expiresAt) < moment()) {
+      this.get("offerResponsePresent")
+        ? this.set("offerNotPresent", false)
+        : this.set("offerNotPresent", true);
     }
     controller.set("model", model);
-    controller.set("offerNotPresent", offerNotPresent);
+    controller.set("offerNotPresent", this.get("offerNotPresent"));
   }
 });
