403a44221b33d0f0e81de6e64e1db47abab61098
swatijadhav <swati.nakshtra17@gmail.com>
2021-08-31 17:48:56 +0530

    GCW-3566 fixed live update of requested-goods

--- app/controllers/orders/goods.js
+++ app/controllers/orders/goods.js
@@ -1,4 +1,4 @@
-import { computed } from "@ember/object";
+import { getWithDefault, computed } from "@ember/object";
 import { alias, notEmpty } from "@ember/object/computed";
 import detail from "./detail";
 import _ from "lodash";
@@ -47,34 +47,41 @@ export default detail.extend({
 
   hasOrderedGoods: notEmpty("order.ordersPackages"),
 
-  orderedGoods: computed("model.packageCategories", function() {
-    const ordersPackages = this.getWithDefault(
-      "order.ordersPackages",
-      []
-    ).filter(
-      req => req.get("state") !== "cancelled" && req.get("quantity") > 0
-    );
-    let res = [];
-    let refs = {};
-    ordersPackages.map(req => {
-      if (!req.get("package.hasSiblingPackages")) {
+  orderedGoods: computed(
+    "model.packageCategories",
+    "model.order.ordersPackages.@each.state",
+    "model.order.ordersPackages.@each.quantity",
+    function() {
+      const ordersPackages = this.getWithDefault(
+        "order.ordersPackages",
+        []
+      ).filter(
+        req => req.get("state") !== "cancelled" && req.get("quantity") > 0
+      );
+
+      let res = [];
+      let refs = {};
+
+      ordersPackages.map(req => {
+        if (!req.get("package.hasSiblingPackages")) {
+          res.push(req);
+          return;
+        }
+        const packageSet = req.get("package.packageSet");
+        const packageSetId = packageSet.get("id");
+        if (refs[packageSetId]) {
+          return; // Already processed
+        }
+        if (this.allPackageSetsOrdered(packageSet, req.get("orderId"))) {
+          refs[packageSetId] = packageSet;
+          res.push(packageSet);
+          return;
+        }
         res.push(req);
-        return;
-      }
-      const packageSet = req.get("package.packageSet");
-      const packageSetId = packageSet.get("id");
-      if (refs[packageSetId]) {
-        return; // Already processed
-      }
-      if (this.allPackageSetsOrdered(packageSet, req.get("orderId"))) {
-        refs[packageSetId] = packageSet;
-        res.push(packageSet);
-        return;
-      }
-      res.push(req);
-    });
-    return _.map(res, record => {
-      return { record };
-    });
-  })
+      });
+      return _.map(res, record => {
+        return { record };
+      });
+    }
+  )
 });
