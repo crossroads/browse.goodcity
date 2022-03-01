652352d79c0a846ffab900b5c22899b5dcef83cf
Steve Kenworthy <steveyken@gmail.com>
2022-03-01 10:59:56 +0800

    Ensure bookAppointment is set/cleared appropriately

--- app/routes/authorize.js
+++ app/routes/authorize.js
@@ -2,6 +2,11 @@ import { resolve } from "rsvp";
 import Route from "@ember/routing/route";
 
 export default Route.extend({
+  queryParams: { bookAppointment: "false" },
+  beforeModel(params) {
+    this.set("queryParams", params.queryParams);
+  },
+
   loadIfAbsent(modelName, id) {
     let store = this.get("store");
     let cachedRecord = store.peekRecord(modelName, id);
@@ -14,9 +19,10 @@ export default Route.extend({
   beforeModel(transition) {
     if (!this.session.get("isLoggedIn")) {
       transition.abort();
+      var queryParams = this.get("queryParams");
       var loginController = this.controllerFor("login");
       loginController.set("attemptedTransition", transition);
-      this.transitionTo("login");
+      this.transitionTo("login", { queryParams: queryParams });
       return false;
     }
     return true;
